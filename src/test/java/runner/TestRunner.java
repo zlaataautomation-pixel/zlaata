4cb822d103a8b26f06b98bd1130ed5e6451820b0
Ranjith <Ranjith@192.168.1.34>
2025-09-04 10:32:00 +0530

    Done committed 10:31 AM

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -15,7 +15,8 @@ import java.util.logging.Logger;
 features = {"src/test/resources/features"},
 glue = "stepDef",	
 monochrome = true,	
-dryRun = false
+dryRun = false,
+tags="@TC_UI_Zlaata_COP_01"
 
 )
 
